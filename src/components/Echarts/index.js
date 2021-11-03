557b05aa023b780cebff54dd2d0212fff6d91513
lay <>
2021-11-03 08:22:10 +0800

    更换钩子

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   }
   
 
-  componentWillReceiveProps(nextProps) {
+  componentDidUpdate(nextProps) {
     if(nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
